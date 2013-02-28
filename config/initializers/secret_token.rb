5fb1894be2861a8d40a8b84b80b2634125a6099d
Sathiya Subramanian <sathiya@gmail.com>
2013-02-27 16:29:53 -0800

    firstgenesis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'a4221ac08d1f2778801a233269e0555550cc2c87afea29147e1448e1b03d61c159420d5f9e5d79457bd5846cf51f9b9b26ca7095230c9ec45dd97a82946c6188'
